475a6eccb140876372874b8ed0bb2e4562cff298
Amodu Kehinde <amodukehindee@gmail.com>
2022-02-14 11:06:59 +0100

    Delete src directory

--- src/ReduxContianer/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import personalReducer from './PersonalRedux/PersonalReducer';
-
-
-const rootReducer = combineReducers({
-   personal: personalReducer
-})
-
-export default rootReducer;
