a5b9f0ddd7b52360748268b2d5620b4e21d01bed
Sterling <sterling@Sterlings-MacBook-Pro.local>
2022-04-01 09:18:22 +0100

    persist the store

--- src/ReduxContianer/store.js
+++ src/ReduxContianer/store.js
@@ -1,12 +1,24 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-// import {persistStore} from 'redux-persist'
-import { logger } from 'redux-logger';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { logger } from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+
+const persistConfig = {
+  key: "persist-key",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = [thunk, logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(...middleware)
+);
+const persistedStore = persistStore(store);
+export { persistedStore };
 
 // export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './ReduxContianer/store';
-import ErrorBoundary from './ErrorBoundary'
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { persistedStore, store } from "./ReduxContianer/store";
+import ErrorBoundary from "./ErrorBoundary";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<ChakraProvider>
-				{/* <ErrorBoundary> */}
-				<App />
-				{/* </ErrorBoundary> */}
-			</ChakraProvider>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate  loading = {null} persistor={persistedStore}>
+        <ChakraProvider>
+          {/* <ErrorBoundary> */}
+          <App />
+          {/* </ErrorBoundary> */}
+        </ChakraProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
