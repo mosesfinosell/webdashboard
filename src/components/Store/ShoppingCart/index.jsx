3f5e454ef1b5ea305bc78cf1c4a43cf2a427198d
fiyintheslim <fiyinoyekunle@gmail.com>
2022-07-04 17:53:46 +0100

    cart

--- src/components/Store/ShoppingCart/index.jsx
+++ src/components/Store/ShoppingCart/index.jsx
@@ -9,18 +9,21 @@ import {CartContainer, Cart } from "./styles"
 const Index = ({visible, setVisible}) => {
     const [checkOut, setCheckOut] = useState(false)
     const store = useSelector((state)=>state.shoppingCart)
+    
+    const handleVisibility = (e)=>{
+      e.stopPropagation()
+      
+    }
   return (
-    <CartContainer visible={visible}>
-        <Cart>
+    <CartContainer onClick={()=>setVisible(false)} visible={visible}>
+        <Cart onClick={(e)=>handleVisibility(e)}>
             <FaTimes onClick={()=>setVisible(false)} />
             <h3 className="title">Cart</h3>
             {!checkOut ? 
              <Items setCheckOut={setCheckOut} />
              :
              <CheckOut />
-            }
-               
-            
+            }  
         </Cart>
     </CartContainer>
   )
